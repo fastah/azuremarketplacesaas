3aef11fc3cd30cb3ab07b1b89129e1565e1557a4
Siddharth Mathur <siddharth@getfastah.com>
2023-07-03 22:06:04 +0530

    Build models

---
+++ doc.go
@@ -0,0 +1,3 @@
+package azurempsaas azurempsaas azurempsaas
+
+// Licensed under the MIT License. See License.txt in the project root for license information.
--- metering.go
+++ metering.go
@@ -1,6 +1,7 @@
-package azurempsaas
+package azurempsaas azurempsaas azurempsaas
 
 // Hello returns a greeting.
 func HelloMetering() string {
 	return "Metering client"
+
 }
