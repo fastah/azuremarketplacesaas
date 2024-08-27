7da541cff56cddd84e9e2723b4108bb806dd33c7
Siddharth Mathur <siddharth@getfastah.com>
2024-08-27 21:57:29 +0530

    Environment variable sample

---
+++ env.sh
@@ -0,0 +1,4 @@
+export AZURE_TENANT_ID=
+export AZURE_CLIENT_ID=
+export AZURE_CLIENT_SECRET=
+                                                                                                                                                                        
